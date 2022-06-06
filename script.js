a497b4fd00cc1e611e78e2b2ebf10f65155db841
Plasnid <stuart@sackler.ca>
2022-06-06 15:44:45 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// ! pointer eventts
+
+/**
+ * * first touch has isPrimary = true, others are false
+ * * pointer Id is a unique touchpoint for each touch
+ * * height and width relate to how much of the screen you have touched
+ * * tilt and pressure are also tracked if you are using a pen device
+ */
+
+const video = document.querySelector('.video');
+const timeline = document.querySelector(".timeline");
